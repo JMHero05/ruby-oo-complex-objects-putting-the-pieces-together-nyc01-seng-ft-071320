25de8711f3644ab6592fd8e6da5f99f6c3cb820c
JMHero05 <jake.mills.05@gmail.com>
2020-07-14 21:17:14 -0400

    Lab Completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+require 'pry'
+
+
+class Shoe
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        if @condition == "old"
+            @condition = "new"
+        end
+        puts "Your shoe is as good as new!"
+    end
+
+end
+
+# binding.pry
